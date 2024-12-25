eecb995f09bf2285d55fe51df64780a5a4ff0db3
abracal <abravoc@iesrafaelalberti.es>
2024-12-25 10:53:33 +0100

    finalizado #1

--- hundirflota/build.gradle.kts
+++ hundirflota/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 group = "org.alejandro.kotlin"
@@ -10,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // para las librerias kotlinx
     testImplementation(kotlin("test"))
 }
 
---
+++ hundirflota/src/main/kotlin/identificarcarpeta.kt
@@ -0,0 +1,21 @@
+package org.alejandro.kotlin
+import java.io.File
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+/**
+ * Clase para guardar el directorio y trabajar con el
+ * @param rutaDirectorio Es la ruta absoluta del directorio al que se va a comprobar si existe o no
+ */
+class Directorio(val rutaDirectorio : String){
+    val ruta = File(rutaDirectorio)
+    /** Metodo encargado de revisar si esa ruta del directorio existe o no
+    * Return: Devuelve true en caso de que exista y false en caso de que no exista
+     */
+    fun revisarDirectorio () : Boolean{
+        val existencia = this.ruta.isDirectory()
+        return existencia
+    }
+}
+
+
+
