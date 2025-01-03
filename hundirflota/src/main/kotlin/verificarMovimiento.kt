4264e6647b6bb0bc503420fcd08beb7d73957227
arodovi852 <arodriguezo@iesrafaelalberti.es>
2025-01-03 18:25:36 +0100

    Delete hundirflota/src/main/kotlin/verificarMovimiento.kt

--- hundirflota/src/main/kotlin/verificarMovimiento.kt
+++
@@ -1,9 +0,0 @@
-fun verificarMovimiento(movimientos: mutableSet, coordenada: list<Int>): Boolean {
-    var validez = true
-    for (i in movimientos){
-        if (movimientos[i] == coordenada){
-            validez = false
-        }
-    }
-    return validez
-}
