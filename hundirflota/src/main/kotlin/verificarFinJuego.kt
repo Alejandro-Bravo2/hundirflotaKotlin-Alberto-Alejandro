ec5e4733189d0ee1960bc5b2fc9ea63fcd1e4efa
arodovi852 <arodriguezo@iesrafaelalberti.es>
2025-01-03 18:25:28 +0100

    Delete hundirflota/src/main/kotlin/verificarFinJuego.kt

--- hundirflota/src/main/kotlin/verificarFinJuego.kt
+++
@@ -1,9 +0,0 @@
-fun verificarFinJuego(jugador: MutableSet<String>): Boolean {
-    var condicion: Boolean = true
-    for (i in jugador["tablero"]) {
-        if(jugador["tablero"][i] == "B") {
-            condicion = false
-        }
-    }
-    return condicion
-}
