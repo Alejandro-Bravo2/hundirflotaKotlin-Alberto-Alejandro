86dfcc996b786a25fe090d9ae6e275f91670b7df
arodovi852 <arodriguezo@iesrafaelalberti.es>
2025-01-03 18:25:03 +0100

    Delete hundirflota/src/main/kotlin/ataque.kt

--- hundirflota/src/main/kotlin/ataque.kt
+++
@@ -1,9 +0,0 @@
-fun ataque(): List<Int> {
-    println("Introduzca una coordenada para atacar a un barco en formato => x, y")
-    var coordenadaDeAtaqueAux = readln().split(",")
-    var coordenadaDeAtaque: List<Int> = listOf()
-    for (k in coordenadaDeAtaqueAux) {
-        coordenadaDeAtaque.append(coordenadaDeAtaqueAux[k].toInt())
-    }
-    return coordenadaDeAtaque
-}
